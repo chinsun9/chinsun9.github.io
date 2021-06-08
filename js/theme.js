e96bf9eb554fba100324965f2ea244e8b8aaf30a
chinsun9 <chin_sung@naver.com>
2021-06-08 11:59:22 +0900

    Site updated: 2021-06-08 11:59:18

--- js/theme.js
+++ js/theme.js
@@ -1,51 +1,80 @@
 const btn = document.getElementById('toggleTheme');
 
+// utterances
+function setUtterancesTheme(theme) {
+  const utterances = document.querySelector('.utterances iframe');
+
+  const message = {
+    type: 'set-theme',
+    theme: theme === 'dark' ? 'github-dark' : 'github-light',
+  };
+
+  utterances.contentWindow.postMessage(message, 'https://utteranc.es');
+}
+
+// basic
+
 function initTheme() {
-    // chk local storage
-    const theme = localStorage.getItem("theme");
-    if (theme === "dark") {
-        document.body.classList.add("dark");
-        btn.innerText = '🌙'
-        return;
-    }
-    if (theme === "light") {
-        btn.innerText = '🌞'
-        return;
-    }
+  // chk local storage
+  const theme = localStorage.getItem('theme');
+  if (theme === 'dark') {
+    document.body.classList.add('dark');
+    setUtterancesTheme('dark');
+    btn.innerText = '🌙';
+    return;
+  }
+  if (theme === 'light') {
+    btn.innerText = '🌞';
+    setUtterancesTheme('light');
+    return;
+  }
 
-    //   visit first time
-    const isDark = window.matchMedia("(prefers-color-scheme: dark)");
-    if (isDark) document.body.classList.add("dark");
+  //   visit first time
+  const isDark = window.matchMedia('(prefers-color-scheme: dark)');
+  if (isDark) {
+    document.body.classList.add('dark');
+  }
 
-    localStorage.setItem("theme", isDark ? "dark" : "light");
-    btn.innerText = isDark ? "🌙" : "🌞"
+  localStorage.setItem('theme', isDark ? 'dark' : 'light');
+  btn.innerText = isDark ? '🌙' : '🌞';
+  setUtterancesTheme(
+    document.body.classList.contains('dark') ? 'dark' : 'light'
+  );
 }
 
 function toggleTheme() {
-    console.log(`toggle theme`);
-    document.body.classList.toggle("dark");
-    localStorage.setItem("theme", document.body.classList.contains('dark') ? "dark" : "light");
-    btn.innerText = document.body.classList.contains('dark') ? "🌙" : "🌞"
+  console.log(`toggle theme`);
+  document.body.classList.toggle('dark');
+
+  localStorage.setItem(
+    'theme',
+    document.body.classList.contains('dark') ? 'dark' : 'light'
+  );
+  btn.innerText = document.body.classList.contains('dark') ? '🌙' : '🌞';
+  setUtterancesTheme(
+    document.body.classList.contains('dark') ? 'dark' : 'light'
+  );
 }
 
 window
-    .matchMedia("(prefers-color-scheme: dark)")
-    .addEventListener("change", (e) => {
-        const newColorScheme = e.matches ? "dark" : "light";
-        localStorage.setItem("theme", newColorScheme);
+  .matchMedia('(prefers-color-scheme: dark)')
+  .addEventListener('change', (e) => {
+    const newColorScheme = e.matches ? 'dark' : 'light';
+    localStorage.setItem('theme', newColorScheme);
 
-        if (newColorScheme === "dark") {
-            document.body.classList.add("dark");
-            btn.innerText = '🌙'
-            return;
-        }
-        document.body.classList.remove("dark");
-        btn.innerText = '🌞'
-    });
+    if (newColorScheme === 'dark') {
+      document.body.classList.add('dark');
+      btn.innerText = '🌙';
+      setUtterancesTheme('dark');
+      return;
+    }
+    document.body.classList.remove('dark');
+    btn.innerText = '🌞';
+    setUtterancesTheme('light');
+  });
 
 btn.addEventListener('click', function (event) {
-    toggleTheme()
+  toggleTheme();
 });
-
 
 initTheme();
